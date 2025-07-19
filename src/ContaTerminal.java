be73bbfd8a76308fb28bb496677144bc4daf3cac
Matheus Zanqueta <164265012+m-zanqueta@users.noreply.github.com>
2025-07-19 15:33:29 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Por favor, digite o número da conta:");
+        var conta = scanner.nextInt();
+
+        System.out.println("Por favor, digite o número a agência:");
+        var agencia = scanner.nextInt();
+
+        System.out.println("Por favor, digite seu nome:");
+        var nome = scanner.next();
+
+        System.out.println("Por favor, digite o saldo:");
+        var saldo = scanner.nextInt();
+
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %s já está dispoível para saque", nome, agencia, conta, saldo);
     }
 }
